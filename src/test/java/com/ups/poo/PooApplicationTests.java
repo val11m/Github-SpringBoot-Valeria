bb5ce33bab5b8036a60b5a9532efcbeae152b146
val11m <160269759+val11m@users.noreply.github.com>
2024-03-11 15:45:51 -0500

    Update PooApplicationTests.java

--- src/test/java/com/ups/poo/PooApplicationTests.java
+++ src/test/java/com/ups/poo/PooApplicationTests.java
@@ -1,1 +1,13 @@
+package com.ups.poo;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
